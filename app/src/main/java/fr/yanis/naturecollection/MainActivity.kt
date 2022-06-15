7dbaf415ccbd79508a07e7b618ceea68e56deaea
yanis <afifyanis@gmail.com>
2022-06-15 11:32:17 +0200

    Create project insert all default layout

---
+++ app/src/main/java/fr/yanis/naturecollection/MainActivity.kt
@@ -0,0 +1,18 @@
+package fr.yanis.naturecollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.yanis.naturecollection.fragments.HomeFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Injecter le fragment dans notre boite (fragment_container)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+}
